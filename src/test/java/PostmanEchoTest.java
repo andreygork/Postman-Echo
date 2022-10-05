958e1c9d8ddd1fd1d0754cfecef1014d5d59337a
andreygork <agorkaev99@gmail.com>
2022-10-05 18:06:20 +0300

    Add Test

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnValue() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello"))
+        ;
+    }
+}
